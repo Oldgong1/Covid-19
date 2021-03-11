c40bb1c8e9035eca398aa23b3ffa6dbbf1b6092c
lunkurli <lunkurli@gmail.com>
2021-03-11 18:33:04 +0800

    modified

--- covid/covid/settings.py
+++ covid/covid/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '$n7r63m^97pq&zufn7)$27ucxb+&-f$9*oaefebxcyg8k*vc%u'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
